78f8f6a07ace6675da4a53ae1ceeff9bd6f9b290
Tului Gantulga <tgantulga@ucdavis.edu>
2023-07-11 20:14:21 -0700

    added a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css';
 import ProblemOne from './problem1';
 import ProblemTwo from './problem2';
+import { Button, Grid } from '@mui/material';
 
 const router = createBrowserRouter([
   {
@@ -20,7 +21,27 @@ const router = createBrowserRouter([
 
 const App = () => {
   return (
-    <RouterProvider router={router} />
+    <Grid style={{ width: '100vw' }}>
+      <div style={{ width: '100%', height: '50px', borderBottom: '1px solid grey' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+          <Button
+            onClick={() => {
+              window.location.pathname = '/problem-one'
+            }}
+          >
+            Problem 1
+          </Button>
+          <Button
+            onClick={() => {
+              window.location.pathname = '/problem-two'
+            }}
+          >
+            Problem 2
+          </Button>
+        </div>
+      </div>
+      <RouterProvider router={router} />
+    </Grid>
   );
 }
 
