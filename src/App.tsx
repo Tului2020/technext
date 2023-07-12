152c47bf1440abe9404fc0adcd5cb37af5c4b38a
Tului Gantulga <tgantulga@ucdavis.edu>
2023-07-11 19:06:26 -0700

    re-arranged for problem 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,21 @@
-import { useState } from 'react';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
-import { Button, CircularProgress, Grid, TextField } from '@mui/material';
-import ResultTable from './table';
+import ProblemOne from './problem1';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to='/problem-one' replace />
+  },
+  {
+    path: '/problem-one',
+    element: <ProblemOne />,
+  },
+]);
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [searchPhrase, setSearchPhrase] = useState('laser');
-
-  const handleQuery = () => {
-    if (searchPhrase) {
-      setLoading(true);
-      fetch('https://testtechnext1-pearl118.b4a.run/search/api/query/?query=' + searchPhrase)
-        .then(res => res.json())
-        .then((res: any) => setData(res))
-        .catch((err) => console.error(err))
-        .finally(() => setLoading(false));
-    }
-  }
-
   return (
-    <Grid container className='App' spacing={3}>
-      <Grid xs={12} item style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-        <TextField
-          placeholder='Search Here...'
-          size='small'
-          value={searchPhrase}
-          onChange={(e) => setSearchPhrase(e.target.value)}
-          style={{ marginRight: '15px' }}
-          onKeyDown={(e) => (e.key === 'Enter' && handleQuery())}
-        />
-
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          {loading ?
-            <CircularProgress
-              size={25}
-            /> :
-            <Button
-              onClick={handleQuery}
-              disabled={loading}
-            >
-              Search
-            </Button>
-          }
-        </div>
-      </Grid>
-
-      <Grid xs={12} item>
-        <ResultTable
-          data={data}
-        />
-      </Grid>
-
-    </Grid>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/problem1/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { Button, CircularProgress, Grid, TextField } from '@mui/material';
+import ResultTable from './table';
+
+
+const ProblemOne = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchPhrase, setSearchPhrase] = useState('laser');
+
+  const handleQuery = () => {
+    if (searchPhrase) {
+      setLoading(true);
+      fetch('https://testtechnext1-pearl118.b4a.run/search/api/query/?query=' + searchPhrase)
+        .then(res => res.json())
+        .then((res: any) => setData(res))
+        .catch((err) => console.error(err))
+        .finally(() => setLoading(false));
+    }
+  }
+
+  return (
+    <Grid container className='App' spacing={3}>
+      <Grid xs={12} item style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+        <TextField
+          placeholder='Search Here...'
+          size='small'
+          value={searchPhrase}
+          onChange={(e) => setSearchPhrase(e.target.value)}
+          style={{ marginRight: '15px' }}
+          onKeyDown={(e) => (e.key === 'Enter' && handleQuery())}
+        />
+
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          {loading ?
+            <CircularProgress
+              size={25}
+            /> :
+            <Button
+              onClick={handleQuery}
+              disabled={loading}
+            >
+              Search
+            </Button>
+          }
+        </div>
+      </Grid>
+
+      <Grid xs={12} item>
+        <ResultTable
+          data={data}
+        />
+      </Grid>
+
+    </Grid>
+  );
+}
+
+export default ProblemOne;
